82b89b445335f5476821753ffb8738d9175d9ce3
Rahul Komatineni <rahulk.utd@gmail.com>
2018-06-21 18:30:00 -0500

    Added search bar and actions

---
+++ src/actions/index.js
@@ -0,0 +1,16 @@
+import axios from 'axios';
+
+const API_KEY = 'a14e1f38a30b3ae637d3dd5eb8140465';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city) {
+    const url = `${ROOT_URL}&q=${city},us`;
+    const request = axios.get(url);
+    return {
+        type: FETCH_WEATHER,
+        payload: request
+
+    };
+}
