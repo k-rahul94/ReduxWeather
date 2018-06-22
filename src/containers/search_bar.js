6cfa6ce7ac838bbca108438846a8e90d9464ec0a
Rahul Komatineni <rahulk.utd@gmail.com>
2018-06-21 22:01:19 -0500

    Added redux application state for weather data

--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -3,12 +3,13 @@ import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import { fetchWeather } from '../actions/index';
 
-export default class SearchBar extends Component {
+class SearchBar extends Component {
     constructor(props) {
         super(props);
 
         this.state = { term: '' };
         this.onInputChange = this.onInputChange.bind(this);
+        this.onFormSubmit = this.onFormSubmit.bind(this);
     }
 
     onInputChange(event) {
@@ -19,6 +20,8 @@ export default class SearchBar extends Component {
         event.preventDefault();
 
         // We need to go and fetch weather data
+        this.props.fetchWeather(this.state.term);
+        this.setState({ term: '' });
     }
 
     render() {
@@ -36,3 +39,9 @@ export default class SearchBar extends Component {
         );
     };
 }
+
+function mapDispatchToProps(dispatch) {
+    return bindActionCreators({ fetchWeather }, dispatch);
+}
+
+export default connect(null, mapDispatchToProps)(SearchBar);
